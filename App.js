3b905455fa94c60adb2d2ebeb0e47eb33d1b5ab7
firas hamila <firas.hm.1996@gmail.com>
2024-11-11 20:55:44 +0100

    aa

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 export default function App() {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("Notification recived !!!");
+        console.log(notification.request.content.data.msg);
+      }
+    );
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+  const setNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Look at that notification",
+        body: "I'm so proud of myself!",
+        data: { msg: "Hello", age: 11 },
+      },
+      trigger: { seconds: 3 },
+    });
+    console.log("Notification send !!!");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={styles.button} onPress={setNotificationHandler}>
+        <Text style={styles.btnTxt}>Send Notif</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "red",
+    padding: 10,
+    paddingHorizontal: 30,
+    borderRadius: 12,
+  },
+  btnTxt: {
+    color: "white",
+    fontSize: 24,
   },
 });
